09e6d4d3f24868892d257efdaba2bd9b48cf75e6
ToraSpencer <340473925@qq.com>
2020-10-19 18:06:49 +0800

    2020.10.19星期一下班前

--- eigenStudy/eigenStudy/eigenStudy.cpp
+++ eigenStudy/eigenStudy/eigenStudy.cpp
@@ -8,49 +8,159 @@ using namespace std;
 
 #define PI 3.14159
 
+/*
+	VectorXd类
+			normalized()	返回其归一化向量。
+			normalize()		对象自身进行归一化操作。
+*/
+
+
+
+
+
+static inline VFQuaternion FromAxisAngle(const Vector3d &axis, float angle)
+{
+	return VFQuaternion(cos(angle / 2), axis*sin(angle / 2));
+}
+
 
 // 自定义一个四元数类测试一下
-namespace myQ 
+class VFQuaternion
 {
-	class Quaternion
-	{
-	protected:
-		double w;
-		Vector3d v;
+public:
 
-	public:
-		Quaternion() {}
-
-		Quaternion(double w0, double x0, double y0, double z0) :w(w0), v(x0, y0, z0) {}
-
-		Quaternion(double w0, Vector3d v0) :w(w0), v(v0) {}
-
-
-
-		// 运算符重载
-		//		四元数乘以三维向量——表示该向量v0被该四元素施加旋转操作。
-		//		四元数和旋转角的关系——q = cos(theta/2) + sin(theta/2)*A; 其中A是旋转轴的单位向量。
-		Vector3d operator * (const Vector3d & v0) const
-		{
-			// nVidia SDK implementation
-			Vector3d a, b;
-			a = this->v.cross(v0);
-			b = this->v.cross(a);
-			a *= (2.0f * this->w);
-			b *= 2.0f;
-
-			return (v0 + a + b);
-		}
-
-
-		// 其他方法
-		void disp()
-		{
-			cout << "(" << this->w << ", " << this->v[0] << ", " << this->v[1] << ", " << this->v[2] << ")" << endl;
-		}
-
+// 成员数据
+	union {
+		struct {
+			double    s;			 
+			Vector3d v;		 
+		};
+		struct {	// 注意原项目中是float类型
+			double    w;
+			double    x;
+			double    y;
+			double    z;
+		};
+		struct { double elem[4]; };  
 	};
-}
+
+	static VFQuaternion ZERO;
+	static VFQuaternion IDENTITY;
+
+
+//	构造函数
+	VFQuaternion() {}
+
+	VFQuaternion(double w0, double x0, double y0, double z0) :w(w0), v(x0, y0, z0) {}
+
+	VFQuaternion(double w0, Vector3d v0) :w(w0), v(v0) {}
+
+	VFQuaternion(const VFQuaternion& q) :s(q.s), v(q.v) {}
+
+
+	// 运算符重载
+	//		四元数乘以三维向量——表示该向量v0被该四元素施加旋转操作。
+	//		四元数和旋转角的关系——q = cos(theta/2) + sin(theta/2)*A; 其中A是旋转轴的单位向量。
+	Vector3d operator * (const Vector3d & v0) const
+	{
+		// nVidia SDK implementation
+		Vector3d a, b;
+		a = this->v.cross(v0);
+		b = this->v.cross(a);
+		a *= (2.0f * this->w);
+		b *= 2.0f;
+
+		return (v0 + a + b);
+	}
+
+	// 2.运算符重载
+	VFQuaternion &operator =(const VFQuaternion &q)
+	{
+		s = q.s; v = q.v; return *this;
+	}
+
+
+
+
+	// 其他方法
+	void disp()
+	{
+		cout << "(" << this->w << ", " << this->v[0] << ", " << this->v[1] << ", " << this->v[2] << ")" << endl;
+	}
+
+
+
+
+	static VFQuaternion FromRotateAxis(const Vector3d & orign, const Vector3d & dest)
+	{
+		Vector3d vo, vd;
+		vo = orign.normalized();
+		vd = dest.normalized();
+
+		Vector3d vAxis = vo.cross(vd);
+		double     dot = vo.dot(vd);
+
+
+		if (vAxis.isZero())
+		{
+			if (dot > 0)
+				return IDENTITY;
+			else
+			{
+				Vector3d vtemp(vo * vo);
+
+				if (vtemp.y >= vtemp.x && vtemp.z >= vtemp.x)
+					vtemp = Vector3d(1,0,0);
+				else if (vtemp.x >= vtemp.y && vtemp.z >= vtemp.y)
+					vtemp = Vector3d(0,1,0);
+				else
+					vtemp = Vector3d(0,0,1);
+
+				return VFQuaternion::FromAxisAngle(vo.Cross(vtemp), VF_PI);
+			}
+		}
+
+		return VFQuaternion::FromAxisAngle(vAxis.normalize(), VFMath::ACos(dot).valueRadians());
+	}
+
+
+
+};
+
+VFQuaternion VFQuaternion::ZERO(0.0, 0.0, 0.0, 0.0);
+VFQuaternion VFQuaternion::IDENTITY(1.0, 0.0, 0.0, 0.0);
+
+
+
+
+//	学习项目中的location类的定义，将平移操作和旋转操作都封装到一个类内实现。 
+class VFLocation 
+{
+protected:
+	Vector3d    position;					// 三维向量，表示平移操作。
+	VFQuaternion orientation;				// 四元数，表示旋转操作
+	static const VFLocation ORIGIN;			// 原点，默认构造函数构造，即所有元素都是0；
+
+
+public:
+	VFLocation() {}
+
+	VFLocation(const Vector3d& pos, const VFQuaternion& q):position(pos), orientation(q) {}
+
+	~VFLocation() {}
+
+
+	// 局部坐标的三维向量 → 全局坐标的三维向量
+	Vector3d TransLocalVertexToGlobal(const Vector3d& v) const
+	{
+		// 先旋转再平移。
+		return position + orientation * v;	
+	}
+
+
+
+};
+
 
 
 // 第一个eigen程序
@@ -102,9 +212,19 @@ void test1()
 	double theta = PI/2;
 	Vector3d x_hat(1,0,0);
 	//			注意cos()sin()函数只认弧度。
-	myQ::Quaternion q(cos(theta/2),sin(theta/2)*x_hat);		// 表示x轴的四元数
+	VFQuaternion q(cos(theta/2),sin(theta/2)*x_hat);		// 表示x轴的四元数
 	v2 = q*v1;
 	cout << "v2 == \n" << v2 << endl;
+	cout << "q == " << endl;
+	q.disp();
+
+	// test 
+	cout << endl << endl;
+	Vector3d temp = Vector3d(1,1,1);
+	//cout << temp.normalized() << endl;
+	temp.normalize();
+	cout << temp << endl;
+	//VFQuaternion qtemp = VFQuaternion::From
 
 
 
